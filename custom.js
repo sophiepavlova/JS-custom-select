f4cd3cfacd3bb8488ef496fd6d84f97cc0bc5906
Sophie Pavlova <olni.pavlova@gmail.com>
2022-11-03 09:41:19 -0700

    Start project

---
+++ custom.js
@@ -0,0 +1,11 @@
+class Dropdown {}
+const dropdown = new Dropdown("#dropdown", {
+  items: [
+    { label: "Москва", id: "spb" },
+    { label: "Киев", id: "kiev" },
+    { label: "Одесса", id: "od" },
+    { label: "Николаев", id: "nik" },
+    { label: "Питер", id: "pt" },
+    { label: "Ney York", id: "new" },
+  ],
+});
