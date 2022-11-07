04e9d78f2efb9c9a3f511d5cdcfd71db3a234800
Sophie Pavlova <olni.pavlova@gmail.com>
2022-11-07 10:21:57 -0800

    Added another js file

--- custom.js
+++ custom.js
@@ -1,5 +1,26 @@
 class Dropdown {
-  constructor() {}
+  constructor(selector, options) {
+    this.element = document.querySelector(selector);
+    this.items = options.items;
+    this.element.querySelector(".dropdown__label").innerText =
+      this.items[0].label;
+    console.log(this.element);
+    this.element.addEventListener("click", (event) => {
+      if (event.target.classList.contains("dropdown__label")) {
+        if (this.element.classList.contains("open")) {
+          this.close();
+        } else {
+          this.open();
+        }
+      }
+    });
+  }
+  open() {
+    this.element.classList.add("open");
+  }
+  close() {
+    this.element.classList.remove("open");
+  }
 }
 const dropdown = new Dropdown("#dropdown", {
   items: [
